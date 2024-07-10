2fade981afb15325083e0af2941a490fc0f89e5b
Chris Drexler <ckolumbus@ac-drexler.de>
2024-07-10 11:24:22 +0200

    chg(report): change padding of hour reporting
    ensure that one and two digit duration are displayed aligned

--- utt/report/formatter.py
+++ utt/report/formatter.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 def format_duration(duration: timedelta) -> str:
     total_minutes, _ = divmod(duration.total_seconds(), 60)
     total_hours, remainder_minutes = divmod(total_minutes, 60)
-    formatted_duration = "{hours:.0f}h{minutes:02.0f}".format(hours=total_hours, minutes=remainder_minutes)
+    formatted_duration = "{hours:2.0f}h{minutes:02.0f}".format(hours=total_hours, minutes=remainder_minutes)
     return formatted_duration
 
 
