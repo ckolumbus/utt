f26ec41d50c79b8444bc2736e5db208d711eabd3
Chris Drexler <ckolumbus@ac-drexler.de>
2024-07-10 11:23:27 +0200

    new: enable datafilename config via env var

--- utt/components/data_filename.py
+++ utt/components/data_filename.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import typing
 
-from ..constants import ENTRY_FILENAME
+from ..constants import ENTRY_FILENAME, DATA_FILE_NAME_ENVVAR
 from .data_dirname import DataDirname
 
 DataFilename = typing.NewType("DataFilename", str)
@@ -11,5 +11,9 @@ DataFilename = typing.NewType("DataFilename", str)
 def data_filename(args: argparse.Namespace, data_dirname: DataDirname) -> DataFilename:
     if args.data_filename:
         return args.data_filename
+
+    if DATA_FILE_NAME_ENVVAR in os.environ:
+        data_filename = os.environ[DATA_FILE_NAME_ENVVAR]
+        return data_filename
 
     return DataFilename(os.path.join(data_dirname, ENTRY_FILENAME))
